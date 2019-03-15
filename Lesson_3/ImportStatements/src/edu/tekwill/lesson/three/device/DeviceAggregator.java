1bef9bb4e586ecae90a40ba2504d8217de9228d9
Lazarev Kate <lazarevkat@gmail.com>
2019-03-15 19:02:08 +0200

    import Device

--- Lesson_3/ImportStatements/src/edu/tekwill/lesson/three/device/DeviceAggregator.java
+++ Lesson_3/ImportStatements/src/edu/tekwill/lesson/three/device/DeviceAggregator.java
@@ -1,6 +1,14 @@
 package edu.tekwill.lesson.three.device;
 
+import edu.tekwill.lesson.three.device.model.Device;
+
+
+
 public class DeviceAggregator {
+
+edu.tekwill.lesson.three.device.data.Device modelDevice;
+
+
 
   Device dataDevice;
   Device modelDevice;
